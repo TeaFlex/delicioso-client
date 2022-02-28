34fb5cc8749658720756b8145937602281b63fc3
teaflex <teaflex.dev@hotmail.com>
2022-02-28 14:16:08 +0100

    new structure for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import { Component, ReactNode } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider, RequireAuth } from "react-auth-kit";
 
 import './App.css';
 
-import Home from './pages/Home';
+import Home from './pages/home/Home';
 import Layout from './pages/Layout';
-import NoPage from './pages/NoPage';
+import NoPage from './pages/noPage/NoPage';
+import { refreshApi } from './services/refreshApi';
+import Connection from './pages/connection/Connection';
 
 export default class App extends Component {
     render(): ReactNode {
         return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="book/" element={<NoPage placeholder='book'/>} />
-                        <Route path="bookings/" element={<NoPage placeholder='bookings'/>} />
-                        <Route path="profile/" element={<NoPage placeholder='profile'/>} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+            <AuthProvider 
+                authType={'localstorage'} 
+                authName={'_auth'} 
+                refresh={refreshApi}
+                >
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={
+                            <RequireAuth loginPath="/connection">
+                                <Layout />
+                            </RequireAuth>
+                        }>
+                            <Route index element={<Home />} />
+                            <Route path="book/" element={<NoPage placeholder='book'/>} />
+                            <Route path="bookings/" element={<NoPage placeholder='bookings'/>} />
+                            <Route path="profile/" element={<NoPage placeholder='profile'/>} />
+                        </Route>
+                        <Route path='/connection' element={<Connection />}/>
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
         )
     }
 }
