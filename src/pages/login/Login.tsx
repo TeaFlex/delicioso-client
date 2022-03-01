9437b71b9b49bc7127ff75d4c55c645367c81ba5
teaflex <teaflex.dev@hotmail.com>
2022-03-01 12:39:04 +0100

    Class component to hook for login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,85 +1,66 @@
-import { withSignIn } from "react-auth-kit";
-import { Component, ReactNode } from "react";
-import axios, { AxiosError }from "axios";
-import { api_auth, refresh_token_expiration, token_expiration } from "../../config";
+import axios, { AxiosError } from "axios";
+import { useState } from "react";
+import { useSignIn } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
 import Form from "../../components/form/Form";
+import { api_auth, refresh_token_expiration, token_expiration } from "../../config";
 import './Login.css';
 
+interface LoginProps {}
 
-interface LoginForm {
-    username: string;
-    password: string;
-}
+const Login = (props: LoginProps): JSX.Element => {
+    
+    const [errors, setErrors] = useState<{[key: string]: string[]} | undefined>();
+    const navigate = useNavigate();
+    const signIn = useSignIn();
 
-interface LoginState {
-    errors?: {[key: string]: string[]};
-}
-
-interface LoginProps {
-    signIn: (...args: any) => boolean;
-}
-
-class Login extends Component<LoginProps, LoginState>{
-
-    form: LoginForm = {
+    let form = {
         username: "",
-        password: ""
+        password: "",
     };
 
-    state: Readonly<LoginState> = {
-        errors: undefined,
-    }
-
-    loginHandler = async () => {
+    const loginHandler = async () => {
         try {
-            const res = await axios.post(api_auth, this.form);
+            const res = await axios.post(api_auth, form);
             
-            this.setState({
-                errors: undefined,
-            });
+            setErrors(undefined);
             
-            const auth = this.props.signIn({
+            const auth = signIn({
                 token: res.data.access,
                 tokenType: "Bearer",
                 refreshToken: res.data.refresh,
                 refreshTokenExpireIn: refresh_token_expiration,
                 expiresIn: token_expiration,
             });
-
-            
+            navigate('/');
             
         } catch (error) {
             const res = (error as AxiosError).response;
-            this.setState({
-                errors: res?.data,
-            });
+            setErrors(res?.data);
         }
-        
     };
 
-    fieldHandler = (e: any) => {
+    const fieldHandler = (e: any) => {
         const o: any = {};
         o[e.target.name] = (e.target.value as string).trim();
-        this.form = {...this.form, ...o};
+        form = {...form, ...o};
     };
 
-    render(): ReactNode {
-        return (
-            <div className="center-window full-h" id="connection">
-                <Form 
-                    title="Login" 
-                    onSubmit={this.loginHandler} 
-                    preventDefault={true}
-                    errors={this.state.errors}
-                >
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" name="username" onChange={this.fieldHandler}/>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" onChange={this.fieldHandler}/>
-                </Form>
-            </div>
-        )
-    }
-}
+    return (
+        <div className="center-window full-h" id="connection">
+            <Form 
+                title="Login" 
+                onSubmit={loginHandler} 
+                preventDefault={true}
+                errors={errors}
+            >
+                <label htmlFor="username">Username:</label>
+                <input type="text" name="username" onChange={fieldHandler}/>
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" onChange={fieldHandler}/>
+            </Form>
+        </div>
+    );
+};
 
-export default withSignIn(Login) as any;
+export default Login;
