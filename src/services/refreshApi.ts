4e5c999624e10cd78056190d8004e9a752776f73
teaflex <teaflex.dev@hotmail.com>
2022-03-01 21:26:17 +0100

    token refresh fixed

--- src/services/refreshApi.ts
+++ src/services/refreshApi.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { createRefresh, useAuthHeader } from "react-auth-kit";
+import { api_refresh } from "../config";
 
 export const refreshApi = createRefresh({
     interval: 2,
@@ -11,20 +12,20 @@ export const refreshApi = createRefresh({
           authUserState
         }) => {
         try {
-          const {data} = await axios.post('/api/v1/token',
+          const {data} = await axios.post(api_refresh,
             {
               refresh: refreshToken,
               oldAuthToken: authToken,
             }
           );
+          console.log("refresh success");
           return {
             isSuccess: true,
             newAuthToken: data.access,
             newAuthTokenExpireIn: data.newAuthTokenExpireIn
           }
         } catch (error) {
-          console.log("e");
-          
+          console.log("bad refresh");
           console.error(error);
           return {
             isSuccess: false,
